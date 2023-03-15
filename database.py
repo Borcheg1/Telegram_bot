1c7f35011e2f97cc062abfd4e0f893f3eca384e4
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-03-15 22:34:01 +0300

    update database.py (deleted return where it doesnt need; fixed getting data from cursor, now using fetchone)

--- database.py
+++ database.py
@@ -20,33 +20,36 @@ class DataBasePostgres:
 
     def add_user(self, user_id):
         with self.connection:
-            return self.cursor.execute("INSERT INTO users (user_id) VALUES (%s)", (user_id,))
+            self.cursor.execute("INSERT INTO users (user_id) VALUES (%s)", (user_id,))
 
     def check_user_exist(self, user_id):
         with self.connection:
-            result = self.cursor.execute("SELECT user_id FROM users WHERE user_id = (%s)", (user_id,))
-            return bool(result)
+            self.cursor.execute("SELECT user_id FROM users WHERE user_id = (%s)", (user_id,))
+            return bool(len(self.cursor.fetchall()))
 
     def set_reg_status(self, user_id, reg_status):
         with self.connection:
-            return self.cursor.execute("UPDATE users SET reg_status = (%s) WHERE user_id = (%s)", (reg_status, user_id,))
+            self.cursor.execute("UPDATE users SET reg_status = (%s) WHERE user_id = (%s)", (reg_status, user_id,))
 
     def check_reg_status(self, user_id):
         with self.connection:
-            return self.cursor.execute("SELECT reg_status FROM users WHERE user_id = (%s)", (user_id,))
+            self.cursor.execute("SELECT reg_status FROM users WHERE user_id = (%s)", (user_id,))
+            return self.cursor.fetchone()[0]
 
     def set_name(self, user_id, name):
         with self.connection:
-            return self.cursor.execute("UPDATE users SET name = (%s) WHERE user_id = (%s)", (name, user_id,))
+            self.cursor.execute("UPDATE users SET name = (%s) WHERE user_id = (%s)", (name, user_id,))
 
     def get_name(self, user_id):
         with self.connection:
-            return self.cursor.execute("SELECT name FROM users WHERE user_id = (%s)", (user_id,))
+            self.cursor.execute("SELECT name FROM users WHERE user_id = (%s)", (user_id,))
+            return self.cursor.fetchone()[0]
 
     def set_email(self, user_id, email):
         with self.connection:
-            return self.cursor.execute("UPDATE users SET email = (%s) WHERE user_id = (%s)", (email, user_id,))
+            self.cursor.execute("UPDATE users SET email = (%s) WHERE user_id = (%s)", (email, user_id,))
 
     def get_email(self, user_id):
         with self.connection:
-            return self.cursor.execute("SELECT email FROM users WHERE user_id = (%s)", (user_id,))
+            self.cursor.execute("SELECT email FROM users WHERE user_id = (%s)", (user_id,))
+            return self.cursor.fetchone()[0]
